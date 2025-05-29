638e8e4501aab3f762af79c7f9a693f248f373ab
ivoasf <ivoasfarias@gmail.com>
2025-05-29 15:27:59 +0100

    update

--- game_demo.py
+++ game_demo.py
@@ -1,30 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu May 27 07:35:58 2021
-
-"""
 
 import matplotlib.pyplot as plt
 from snake_game import SnakeGame
 
-def plot_board(file_name,board,text=None):
+
+""" function to plot the game board in a png file """
+def plot_board(file_name, board, text=None):
     plt.figure(figsize=(10,10))
     plt.imshow(board)
     plt.axis('off')
+
     if text is not None:
         plt.gca().text(3, 3, text, fontsize=45,color = 'yellow')
-    plt.savefig(file_name,bbox_inches='tight')
+
+    plt.savefig(file_name, bbox_inches='tight')
     plt.close()
 
+
+""" funtion to run a demo of the snake game """
 def snake_demo(actions):
-    game = SnakeGame(30,30,border=1)
-    board,reward,done,info = game.reset()    
-    action_name = {-1:'Turn left',0:'Straight ahead',1:'Turn right'}    
-    plot_board('0.png',board,'Start')
-    for frame,action in enumerate(actions):
-        board,reward,done,info = game.step(action)
-        plot_board(f'{frame+1}.png',board,action_name[action])
-        
-snake_demo([0,1,0,-1])
+    game = SnakeGame(30, 30, border=1) # 30x30 board with a border of 1 pixel
     
+    # reset the game and get the initial board state
+    board, reward, done, info = game.reset() 
+    plot_board('0.png', board, 'Start')
+
+    action_name = {-1:'Turn Left', 0:'Straight Ahead', 1:'Turn Right'} # label each action
+
+    # execute the actions and get the resulting board state
+    for frame, action in enumerate(actions):
+        board, reward, done, info = game.step(action)
+        plot_board(f'{frame+1}.png', board, action_name[action])
+
+
+"""
+    -1 = turn left
+    0  = straight ahead
+    1  = turn right
+"""
+snake_demo([0, 1, 0, -1])
